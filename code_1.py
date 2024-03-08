45275f468b7e4143f939687f001d85d452fe7b5d
wissseman777 <161206102+wissseman777@users.noreply.github.com>
2024-03-08 05:49:13 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+    const data = generateRandomData();
+main();
+}
+}
+
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+function generateRandomData() {
+function main() {
+    const data = [];
+    return data;
+
+    data.forEach(item => {
+    }
